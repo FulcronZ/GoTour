cfdb32e0cebbfd7b43ae80ae16e85906ed394056
Pipat Methavanitpong <pipat1010@gmail.com>
2015-06-27 03:00:22 +0900

    Check goroutine aliveness by a channel as semaphore

--- 74-exercise-web-crawler.go
+++ 74-exercise-web-crawler.go
@@ -11,38 +11,60 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+var busyCrawling chan bool // count alive goroutines
 var crawledURLs map[string]bool
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
 	if depth <= 0 {
 		return
 	}
+
+	var isRoot bool
+	if len(crawledURLs) == 0 {
+		isRoot = true
+	}
+
 	body, urls, err := fetcher.Fetch(url)
-	crawledURLs[url] = true
-	if err != nil {
+	switch err {
+	case nil:
+		crawledURLs[url] = true
+		fmt.Printf("found: %s %q\n", url, body)
+		for _, u := range urls {
+			// Check if crawled before
+			if _, crawled := crawledURLs[u]; !crawled {
+				busyCrawling <- true // gen a token
+				go Crawl(u, depth-1, fetcher)
+			}
+		}
+	default:
+		crawledURLs[url] = false
 		fmt.Println(err)
+	}
+
+	switch isRoot {
+	case true:
+		// TODO: Find a better way to wake up upon empty channel
+		for len(busyCrawling) > 0 {
+			time.Sleep(100 * time.Millisecond)
+		}
+		close(busyCrawling)
+		return
+	case false:
+		<-busyCrawling // return a token
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		// Check if crawled before
-		if _, crawled := crawledURLs[u]; !crawled {
-			go Crawl(u, depth-1, fetcher)
-		}
-	}
-	return
 }
 
 func main() {
+	// TODO: Find a way round to overcome this limitation
+	// A channel is used to check if all gorountines finished
+	// This limits the concurrency to the size of channel depth
+	busyCrawling = make(chan bool, 2)
 	crawledURLs = make(map[string]bool)
 	Crawl("http://golang.org/", 4, fetcher)
-
-	// Let goroutines printing before main() exits
-	time.Sleep(500 * time.Millisecond)
 }
 
 // fakeFetcher is Fetcher that returns canned results.
